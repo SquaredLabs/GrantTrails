361fc9c20acdbce5f6ebfb2749969e0995e38960
Brandon Cheng <brandon.cheng@uconn.edu>
2017-09-23 17:52:15 -0400

    Move about description to config file

--- resources/assets/js/config.js
+++ resources/assets/js/config.js
@@ -22,3 +22,25 @@ export const colors = {
   localPoint: 'rgb(226, 93, 93)',
   selected: 'rgb(93, 241, 170)'
 }
+
+// The @markdown comment allows for markdown syntax highlighting when using the
+// language-babel Atom package.
+export const about = /* @markdown */`
+# Husky Trails
+
+While federal, state, and corporate research grants support researchers at an institution, they also play an important role in supporting local economies in a manner that often gets overlooked.
+
+The reason for this is that grant dollars almost never stay completely within an institution, instead they are used to pay for equipment, reagents, consumables, salaries, etc. that are required for actually carrying out the research.
+
+Not surprisingly the companies providing the goods purchased on grants, and the people (graduate students, post-doctoral fellows, technicians, etc.) employed on grant dollars, tend to be located (or live) within relatively close proximity to the institution carrying out the research.
+
+What you’re looking at here is a visualization of where grant dollars received by UConn faculty were spent within the United States between fiscal years 2013 to 2017. Dollars spent within our state are highlighted in red.
+
+Feel free to explore the visualization by typing in a location (city/state or zip), adjusting filters, or just panning and zooming on the map. Most importantly let us know what you think!
+
+## Credit
+
+Husky Trails is an open source project available for universities to showcase their geographic grant distribution. Source code can be found on [GitHub](https://github.com/SquaredLabs/HuskyTrails).
+
+The project built using an extensive library of open data and software. A brief list can be found in the [copyright and license file](https://github.com/SquaredLabs/HuskyTrails/blob/master/LICENSE.md).
+`
