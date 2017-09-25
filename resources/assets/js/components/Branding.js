2289b9bb5bebe7600bd7b08f98f01f77f0d71d96
Brandon Cheng <brandon.cheng@uconn.edu>
2017-09-25 00:12:36 -0400

    Link to squaredlabs.uconn.edu

--- resources/assets/js/components/Branding.js
+++ resources/assets/js/components/Branding.js
@@ -3,7 +3,7 @@ import h from 'h'
 export default () =>
   h('div', { className: 'branding' },
     h('div', { className: 'branding-logos' },
-      h('a', { href: 'https://core.uconn.edu', className: 'squared-labs-logo', target: '_blank', rel: 'noopener noreferrer' },
+      h('a', { href: 'https://squaredlabs.uconn.edu', className: 'squared-labs-logo', target: '_blank', rel: 'noopener noreferrer' },
         h('img', { src: '/img/squared-labs.svg' })),
       h('a', { href: 'https://uconn.edu', className: 'uconn-logo', target: '_blank', rel: 'noopener noreferrer' },
         h('img', { src: '/img/uconn-wordmark-single-blue.png' }))),
